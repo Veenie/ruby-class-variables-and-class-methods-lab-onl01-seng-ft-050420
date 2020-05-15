d030ce0d54164f09c8fb310a5420847f8d0d2e66
Raveen K Sharma <raveen08@gmail.com>
2020-05-15 19:37:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,19 @@ class Song
   
   @@count = 0
   
+  @@artists = []
+  
   attr_accessor :name, :artist, :genre
  
   def initialize
     @count += 1
+    @artists << artist
   end
   
   def self.count
     @@count
   end
+  
+  
+  
 end
