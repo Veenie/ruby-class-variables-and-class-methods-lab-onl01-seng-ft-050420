7bfa1691cd6e27d40e87fdac49e7dae68206f451
Raveen K Sharma <raveen08@gmail.com>
2020-05-15 20:21:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
     genre_count
   end
   
-    def self.artist_count
+  def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist] #if the artist is already in the hash
@@ -52,7 +52,6 @@ class Song
       else
         artist_count[artist] = 1
       end
-    end
     artist_count
   end
 end
